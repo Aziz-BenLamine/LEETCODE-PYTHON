5054e1df1aa9870f269a1afdee0de96c5de4ae0d
Aziz Ben Lamine <azizbenlamine@gmail.com>
2024-12-29 12:59:23 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (16.29%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.py
+++ 3379-score-of-a-string/score-of-a-string.py
@@ -2,8 +2,6 @@ class Solution:
     def scoreOfString(self, s: str) -> int:
         i = 0
         j = 1
-        if(len(s) == 0):
-            return false
         score = 0
         while(j < len(s)):
             score += abs(ord(s[i]) - ord(s[j]))
